77bcb29e60d49f4fd48caca926b4a5a25b327ce4
Orcun Odabas <orcun@odabasi.ch>
2021-05-24 19:29:54 +0200

    init commit and configuration for MySQL

---
+++ src/test/java/ch/odabasi/springbootreactcrudrestapi/SpringbootReactCrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.odabasi.springbootreactcrudrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootReactCrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
